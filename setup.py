31fc4368a7f6b7be3b8552a21f12a5ec88000424
Rick Molenaar <rick_molenaar96@live.nl>
2023-09-25 15:15:20 -0700

    Correct version scheme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AoCHelpers",
-    version="0.2017aN",
+    version="0.2017a2",
     packages=find_packages(),
     install_requires= [
         "requests",
