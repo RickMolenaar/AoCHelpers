86c86574d34c98e6006d3e47633dd26ed0aa916d
Rick Molenaar <rick_molenaar96@live.nl>
2023-09-27 11:45:25 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="AoCHelpers",
-    version="0.2017a4",
+    version="0.2017a5",
     packages=find_packages(),
     install_requires= [
         "requests",
